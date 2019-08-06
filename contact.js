6dd689df7451850c3a24d5cbafc2adea15cb02ac
sai1231 <saishankar@live.in>
2019-08-06 10:42:47 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -19,65 +19,58 @@ function getRamdonNumber() {
 
 function getFinalAttributes(element) {
   let final_attributes = Object.create(null);
-  $.each(element.get(0).attributes, function (i, attrib) {
-    final_attributes[attrib.name] = attrib.value
+  $.each(element.get(0).attributes, function(i, attrib) {
+    final_attributes[attrib.name] = attrib.value;
   });
   return final_attributes;
 }
 
 function getFinalParentAttributes(element) {
   let final_parent_attributes = Object.create(null);
-  $.each(element.parent().get(0).attributes, function (i, attrib) {
-    final_parent_attributes[attrib.name] = attrib.value
+  $.each(element.parent().get(0).attributes, function(i, attrib) {
+    final_parent_attributes[attrib.name] = attrib.value;
   });
   return final_parent_attributes;
-
 }
-$(document).ready(function () {
+$(document).ready(function() {
   let steps = [];
   let step = {};
 
+  $(".btn").mouseenter(function() {
+    this.timer = window.setTimeout(function() {
+      console.log("asdfjahsdfjkls");
+      $.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          userId: "500",
+          title: "sldfgswfljglsdjfl",
+          completed: "false"
+        },
+        function(data, status) {
+          alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+        }
+      );
+    }, 2000);
+  });
 
-    $(".btn").mouseenter(function(){
-      this.timer=window.setTimeout(function() {
-            console.log("asdfjahsdfjkls");
-            $.post("https://jsonplaceholder.typicode.com/posts",
-            {
-              userId:"500",
-              title:"sldfgswfljglsdjfl",
-              completed:"false"
-              },
-            function(data,status){
-              alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-            });
-       }, 2000);
-    });
-    
-     $(".btn").mouseleave(function() {
-      if(this.timer)window.clearTimeout(this.timer);
-     });
+  $(".btn").mouseleave(function() {
+    if (this.timer) window.clearTimeout(this.timer);
+  });
 
-     
-
-
-  
-
-
-  $(document).delegate("button, a", "click", function () {
-    step = Object.create(null)
+  $(document).delegate("button, a", "click", function() {
+    step = Object.create(null);
     let element = $(this);
     step["step_id"] = getRamdonNumber();
     step["action"] = "click";
     step["attributes"] = getFinalAttributes(element);
     step["parent_attributes"] = getFinalParentAttributes(element);
     steps.push(step);
-    localStorage.setItem('siteData', JSON.stringify(steps));
-
+    localStorage.setItem("siteData", JSON.stringify(steps));
   });
 
-  $(document).delegate("input, textarea", "focusout", function (e) {
+  $(document).delegate("input, textarea", "focusout", function(e) {
     let element = $(this);
-    step = Object.create(null)
+    step = Object.create(null);
     step["step_id"] = getRamdonNumber();
     step["action"] = "sendKeys";
     step["attributes"] = getFinalAttributes(element);
@@ -85,40 +78,37 @@ $(document).ready(function () {
     step.value = element.val();
 
     for (var i = 0; i < steps.length; i++) {
-      var isEqual = isEquivalent(steps[i].attributes, step.attributes)
+      var isEqual = isEquivalent(steps[i].attributes, step.attributes);
       if (isEqual) {
-        steps.splice(i, 1)
+        steps.splice(i, 1);
       }
     }
     steps.push(step);
     console.log(steps);
-    localStorage.setItem('siteData', JSON.stringify(steps));
+    localStorage.setItem("siteData", JSON.stringify(steps));
   });
 
-  $(document).delegate("select", "change", function (e) {
+  $(document).delegate("select", "change", function(e) {
     let element = $(this);
-    step = Object.create(null)
+    step = Object.create(null);
     step["step_id"] = getRamdonNumber();
     step["action"] = "select";
     step["attributes"] = getFinalAttributes(element);
     step["parent_attributes"] = getFinalParentAttributes(element);
     step["value"] = element.val();
-    let selectedText = element.find('option:selected').text()
-    step["text"] = selectedText
+    let selectedText = element.find("option:selected").text();
+    step["text"] = selectedText;
     for (var i = 0; i < steps.length; i++) {
-      var isEqual = isEquivalent(steps[i].attributes, step.attributes)
+      var isEqual = isEquivalent(steps[i].attributes, step.attributes);
       if (isEqual) {
-        steps.splice(i, 1)
+        steps.splice(i, 1);
       }
     }
     steps.push(step);
-    localStorage.setItem('siteData', JSON.stringify(steps));
-
-
+    localStorage.setItem("siteData", JSON.stringify(steps));
   });
 
   // $("input").focusout(function () {
 
   // });
-
 });
