2e99ba8652b7ca7ca5bae05ecad74bfc1077be4d
sai1231 <saishankar@live.in>
2019-09-12 15:37:43 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -19,7 +19,7 @@ function getRamdonNumber() {
 
 function getFinalAttributes(element) {
   let final_attributes = Object.create(null);
-  $.each(element.get(0).attributes, function(i, attrib) {
+  $.each(element.get(0).attributes, function (i, attrib) {
     final_attributes[attrib.name] = attrib.value;
   });
   return final_attributes;
@@ -27,48 +27,95 @@ function getFinalAttributes(element) {
 
 function getFinalParentAttributes(element) {
   let final_parent_attributes = Object.create(null);
-  $.each(element.parent().get(0).attributes, function(i, attrib) {
+  $.each(element.parent().get(0).attributes, function (i, attrib) {
     final_parent_attributes[attrib.name] = attrib.value;
   });
   return final_parent_attributes;
 }
-$(document).ready(function() {
+
+var canRead = false;
+
+$("#read_data").click(function () {
+  $(this).text($.trim($(this).text()) === "Read" ? "Reading" : "Read");
+  canRead = canRead === true ? false : true;
+});
+// $(document).mouseenter(function() {
+//   this.timer = window.setTimeout(function() {
+//     console.log("asdfjahsdfjkls");
+
+//     // $.post(
+//     //   "https://jsonplaceholder.typicode.com/posts",
+//     //   {
+//     //     userId: "500",
+//     //     title: "sldfgswfljglsdjfl",
+//     //     completed: "false"
+//     //   },
+//     //   function(data, status) {
+//     //     alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
+//     //   }
+//     // );
+//   }, 1000);
+// });
+
+// $(".btn").mouseleave(function() {
+//   if (this.timer) window.clearTimeout(this.timer);
+// });
+
+$(document).ready(function () {
   let steps = [];
   let step = {};
 
-  $(".btn").mouseenter(function() {
-    this.timer = window.setTimeout(function() {
-      console.log("asdfjahsdfjkls");
-      $.post(
-        "https://jsonplaceholder.typicode.com/posts",
-        {
-          userId: "500",
-          title: "sldfgswfljglsdjfl",
-          completed: "false"
-        },
-        function(data, status) {
-          alert("Data: " + JSON.stringify(data) + "\nStatus: " + status);
-        }
-      );
-    }, 2000);
+  $(document).mouseover(function () {
+    if (canRead ==false){return;}
+    event.stopPropagation();
+    console.log(event.target);
+    $(event.target).hover(
+      function () {
+        $(this).addClass("outline");
+        // console.log($(this).parents());
+        $(this).parent().removeClass("outline");
+        $(this).siblings().removeClass("outline");
+      },
+      function () {
+        $(this).removeClass("outline");
+      }
+    );
   });
 
-  $(".btn").mouseleave(function() {
-    if (this.timer) window.clearTimeout(this.timer);
-  });
-
-  $(document).delegate("button, a", "click", function() {
+  $(document).delegate("button, a", "click", function () {
     step = Object.create(null);
     let element = $(this);
     step["step_id"] = getRamdonNumber();
     step["action"] = "click";
     step["attributes"] = getFinalAttributes(element);
     step["parent_attributes"] = getFinalParentAttributes(element);
-    steps.push(step);
-    localStorage.setItem("siteData", JSON.stringify(steps));
+    setAndGetSiteData(step, steps);
+    //localStorage.setItem("siteData", JSON.stringify(steps));
   });
 
-  $(document).delegate("input, textarea", "focusout", function(e) {
+  $(document).delegate("input, textarea", "focusout", function (e) {
+    let element = $(this);
+    if (element.attr("type") == "radio" || element.attr("type") == "checkbox") {
+      return;
+    }
+    console.log(element.get(0).attributes.type);
+    step = Object.create(null);
+    step["step_id"] = getRamdonNumber();
+    step["action"] = "sendKeys";
+    step["attributes"] = getFinalAttributes(element);
+    step["parent_attributes"] = getFinalParentAttributes(element);
+    step.value = element.val();
+
+    for (var i = 0; i < steps.length; i++) {
+      var isEqual = isEquivalent(steps[i].attributes, step.attributes);
+      if (isEqual) {
+        steps.splice(i, 1);
+      }
+    }
+    setAndGetSiteData(step, steps);
+  });
+
+  $(document).delegate(":radio, :checkbox", "change", function (e) {
     let element = $(this);
     step = Object.create(null);
     step["step_id"] = getRamdonNumber();
@@ -83,12 +130,10 @@ $(document).ready(function() {
         steps.splice(i, 1);
       }
     }
-    steps.push(step);
-    console.log(steps);
-    localStorage.setItem("siteData", JSON.stringify(steps));
+    setAndGetSiteData(step, steps);
   });
 
-  $(document).delegate("select", "change", function(e) {
+  $(document).delegate("select", "change", function (e) {
     let element = $(this);
     step = Object.create(null);
     step["step_id"] = getRamdonNumber();
@@ -104,11 +149,6 @@ $(document).ready(function() {
         steps.splice(i, 1);
       }
     }
-    steps.push(step);
-    localStorage.setItem("siteData", JSON.stringify(steps));
+    setAndGetSiteData(step, steps);
   });
-
-  // $("input").focusout(function () {
-
-  // });
 });
--- stepTable.js
+++ stepTable.js
@@ -1,10 +1,96 @@
-$(document).ready(function () {
+function setAndGetSiteData(step, steps) {
+  steps.push(step);
+  localStorage.setItem("siteData", JSON.stringify(steps));
+  const siteData = JSON.parse(localStorage.getItem("siteData"));
+//   console.log(step);
 
-    var siteData = JSON.parse(localStorage.getItem('siteData'));
-    console.log(siteData);
+  let action = step.action;
+  switch (action) {
+    case "sendKeys":
+      $("#js_recorder").append(
+        "<tr>" +
+          "<th scope='row'>" +
+          step.step_id +
+          "</th>" +
+          "<td>" +
+          step.action +
+          "</td>" +
+          "<td>" +
+          step.attributes.id +
+          "</td>" +
+          "<td>" +
+          step.value +
+          "</td>" +
+          "</tr>"
+      );
+      break;
+    case "select":
 
-    $.each(siteData, function (index, value) {
-        $("#js_recorder").append(index + ": " + value + '<br>');
-    });
+      if ($("#js_recorder").find("#" + step.attributes.id).length) {
+        $("#js_recorder #" + step.attributes.id).remove();
+      }
+      $("#js_recorder").append(
+        "<tr id=" +
+          step.attributes.id +
+          ">" +
+          "<th scope='row'>" +
+          step.step_id +
+          "</th>" +
+          "<td>" +
+          step.action +
+          "</td>" +
+          "<td>" +
+          step.attributes.id +
+          "</td>" +
+          "<td>" +
+          step.value +
+          "</td>" +
+          "</tr>"
+      );
 
-});
+      break;
+    case "click":
+      $("#js_recorder").append(
+        "<tr>" +
+          "<th scope='row'>" +
+          step.step_id +
+          "</th>" +
+          "<td>" +
+          step.action +
+          "</td>" +
+          "<td>" +
+          step.attributes.id +
+          "</td>" +
+          "</tr>"
+      );
+      break;
+  }
+}
+
+
+
+// $.each(siteData, function (index, value) {
+//     $("#js_recorder").append(index + ": " + value.step_id + '<br>');
+// });
+//   $("#js_recorder").append("<p><strong>" + step.step_id + "</p></strong>");
+//   $("#js_recorder").append("<p>" + step.action + "</p>");
+//   $("#js_recorder").append("<p>" + step.value + "</p>");
+//   $("#js_recorder").append(    "<p>" + "attribute_id" + step.attributes.id + "</p>"  );
+//   $("#js_recorder").append(    "<p>" + "attribute_name" + step.attributes.name + "</p>"  );
+
+//   $("#js_recorder").append(
+//     $("<label/>", {
+//       id: step.step_id,
+//       name: step.step_id,
+//       text: step.step_id,
+//       class: "text-white"
+//     }),
+//     $("<input/>", {
+//       type: "text",
+//       id: step.value,
+//       name: step.value,
+//       value: step.value,
+//       placeholder: "Value",
+//       class: "form-control"
+//     }) // Creating Input Element With Attribute.
+//   );
